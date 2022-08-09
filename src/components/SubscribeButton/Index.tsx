87a0c329ef98465b0ed200c4ffb6222b9e5fd32d
dgoncalves <dayana.goncalves@pdmfc.com>
2022-08-09 16:23:35 +0100

    #Payment strapi

--- src/components/SubscribeButton/Index.tsx
+++ src/components/SubscribeButton/Index.tsx
@@ -1,15 +1,41 @@
-import styles from './styles.module.scss'
+import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJS } from '../../services/stripe-js'
 
+import styles from './styles.module.scss'
 interface SubscribeButtonProps {
     priceId: string
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { data: session }  = useSession()
+
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return
+        }
+
+        try {
+            const response = await api.post('/subscribe', {
+                user: session.user
+            })
+
+            const { sessionId } = response.data
+    
+            const stripe = await getStripeJS()
+    
+            await stripe.redirectToCheckout({ sessionId: sessionId })
+        } catch(err) {
+            alert(err.message);
+        }
+    }
 
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe Now
         </button>
